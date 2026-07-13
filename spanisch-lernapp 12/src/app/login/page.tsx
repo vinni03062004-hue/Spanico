7c1def42fbaff2c138cb53ffeced3bfc1eb82a07
vinni03062004-hue <vinni03062004@gmail.com>
2026-07-14 00:10:58 +0200

    Delete spanisch-lernapp 12 directory

--- spanisch-lernapp 12/src/app/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-// Login/Registrierung wurde durch auswählbare Profile ersetzt.
-// Diese Seite leitet nur noch auf die Profil-Auswahl weiter.
-export default function LoginRedirect() {
-  redirect("/profiles");
-}
